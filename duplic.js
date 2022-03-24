742f46090ce3ebd7c2c51b508ea7fd2d61cc89b4
Miguel <miguelestebn@gmail.com>
2022-03-24 18:14:07 +0100

    asdf

--- duplic.js
+++ duplic.js
@@ -1,19 +1,16 @@
 
 const data=[
-  //  00 Título   --- 01 Texto ----                                                 02  archivo.jpg ---- 03 ruta ---                    04 tags
-    ['Juego Planos', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']],
-    ['Simpson Builder 1', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']],
-    ['Simpson Builder 2', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']],
-    ['Interactivo Planos', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']],
-    ['Coloreando Minions', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js','figma']],
-    ['SVG Modifier', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js','figma']],
-    ['Juego Planos', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']],
-    ['Juego Planos', 'Interacivo en el que determinar que tipo de plano aparece', '001.png', 'juego-planos/juegoplanos2.html', ['html','css','js']]
+/* --  00 Título           --- 01 Texto descripcion ----                             ---   02 archivo.jpg  ---- 03 ruta ---                   ----- 04 tags */
+    ['Juego Planos',       'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js']],
+    ['Simpson Builder 1',  'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js']],
+    ['Simpson Builder 2',  'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js']],
+    ['Test Planos',        'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'test-planos/index.html'                , ['html','css','js']],
+    ['Coloreando Minions', 'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js','figma']],
+    ['SVG Modifier',       'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js','figma']],
+    ['Juego Planos',       'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js']],
+    ['Juego Planos',       'Interacivo en el que determinar que tipo de plano aparece',   '001.png',      'juego-planos/juegoplanos2.html'        , ['html','css','js']]
   ]
   
-
-
-
 
 let item ='';
 
